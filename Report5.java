3d36f60bdf8c8e5ed42282d0a3310323c703e1cc
Hikaru Kadekawa <e225734@ie.u-ryukyu.ac.jp>
2022-12-11 20:32:37 +0900

    ステップ２

--- Report5.java
+++ Report5.java
@@ -1,6 +1,11 @@
 public class Report5{
     public static void main(String[] args) {
-        String str = null;
-        System.out.print(str.length());
+        try{
+            String str = null;
+            System.out.print(str.length());
+        }catch(NullPointerException e){
+            System.out.println("NullPointerExceptionが発生しました");
+            System.out.println(e.getMessage());
+        }
     }
 }
